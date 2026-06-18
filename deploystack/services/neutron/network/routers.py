3ee64ab0ee234bb599d1cc1da913292580fe34ea
Sorecchione07435 <Sorecchione07@gmail.com>
2026-06-18 15:21:02 +0200

    update

--- deploystack/services/neutron/network/routers.py
+++ deploystack/services/neutron/network/routers.py
@@ -1,12 +1,11 @@
 import json
 
-from ....utils.core.commands import os_run, os_run_output, run_command
+from ....utils.core.commands import os_run, os_run_output
 from ....utils.config.helpers import parse_bool
-
 from ....utils.core import colors
 
+
 def create_custom_network_router(
-    subnets_list: list,
     routers_list: list,
     provider_networks: list,
     public_bridge: str,
@@ -31,14 +30,16 @@ def create_custom_network_router(
         router_name = f"{network_name}_router"
         subnet_name = f"{network_name}_subnet"
 
+        subnet_info_raw = os_run_output(
+            ["openstack", "subnet", "list", "--name", subnet_name, "-f", "json"],
+            env=env
+        )
+        subnet_list = json.loads(subnet_info_raw) if subnet_info_raw else []
+        subnet_exists = len(subnet_list) > 0
+
         router_exists = any(
             r.get("Name") == router_name or r.get("name") == router_name
             for r in routers_list
-        )
-
-        subnet_exists = any(
-            (s.get("Name") or s.get("name")) == subnet_name
-            for s in subnets_list
         )
 
         if not router_exists:
@@ -73,33 +74,28 @@ def create_custom_network_router(
                 return False
 
         if subnet_exists:
-            subnet_os = json.loads(
-                os_run_output([
-                    "openstack", "subnet", "show",
-                    subnet_name,
+            subnet_id = subnet_list[0].get("ID") or subnet_list[0].get("id")
+
+            ports_raw = os_run_output(
+                [
+                    "openstack", "port", "list",
+                    "--router", router_name,
                     "-f", "json"
-                ], env=env)
+                ],
+                env=env
             )
-
-            subnet_id = subnet_os["id"]
-
-            router_ifaces = json.loads(
-                os_run_output([
-                    "openstack", "router", "show",
-                    router_name,
-                    "-f", "json"
-                ], env=env)
-            )
-
-            interfaces = router_ifaces.get("interfaces_info") or []
+            ports = json.loads(ports_raw) if ports_raw else []
 
             already_attached = any(
-                i.get("subnet_id") == subnet_id
-                for i in interfaces
+                subnet_id in [
+                    fip.get("subnet_id")
+                    for fip in (p.get("Fixed IP Addresses") or p.get("fixed_ips") or [])
+                ]
+                for p in ports
             )
 
             if not already_attached:
-                if not run_command(
+                if not os_run(
                     [
                         "openstack", "router", "add",
                         "subnet", router_name, subnet_name
@@ -108,5 +104,9 @@ def create_custom_network_router(
                     env=env
                 ):
                     return False
+            else:
+                print(
+                    f"{colors.YELLOW}'{subnet_name}' already attached to '{router_name}'{colors.RESET}"
+                )
 
     return True
