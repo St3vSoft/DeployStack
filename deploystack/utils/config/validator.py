5a315255811f1bbec6de452b519d5f21ed26df38
Sorecchione07435 <Sorecchione07@gmail.com>
2026-07-17 10:12:22 +0200

    update

--- deploystack/utils/config/validator.py
+++ deploystack/utils/config/validator.py
@@ -472,8 +472,10 @@ def validate_cinder(config) -> bool:
                 ok = False
 
     target_ip = get(config, "cinder.TARGET_IP_ADDRESS") or ""
-    if target_ip and not validate_ip(target_ip, "cinder.TARGET_IP_ADDRESS") and target_ip != "{network.HOST_IP}":
-        ok = False
+    
+    if target_ip and target_ip != "{network.HOST_IP}":
+        if not validate_ip(target_ip, "cinder.TARGET_IP_ADDRESS"):
+            ok = False
     
     if volume_clear not in ("zero", "shred", "none"):
         print(
