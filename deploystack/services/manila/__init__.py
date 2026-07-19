d047715de9ea3bf712dfbf6c9d16b6002024d642
Sorecchione07435 <Sorecchione07@gmail.com>
2026-07-19 13:33:03 +0200

    update

--- deploystack/services/manila/__init__.py
+++ deploystack/services/manila/__init__.py
@@ -2,7 +2,9 @@ from .backends.generic import run_setup_generic_backend
 from .common import run_setup_common_manila
 from .backends.lvm import run_setup_lvm_backend
 
+from ...utils.config.parser import get
+
 def run_setup_manila(config, env):
-    driver = config.get("manila", {}).get("BACKEND", "generic").lower()
-    driver_fn = run_setup_generic_backend if driver == "generic" else run_setup_lvm_backend
+    driver = (get(config, "manila.BACKEND") or "generic").lower().strip()
+    driver_fn = run_setup_lvm_backend if driver == "lvm" else run_setup_generic_backend
     return run_setup_common_manila(config, driver_fn, env)
